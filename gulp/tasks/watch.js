2df50310cf16c98d637b22c0e4cc6be066489b77
X <deafcatmoan@gmail.com>
2017-06-29 23:39:09 -0400

    Sprite generation complete

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,25 +1,28 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task('watch', ()=> {
+gulp.task('watch', function() {
 
   browserSync.init({
+    notify: false,
     server: {
-    baseDir: "app"
+      baseDir: "app"
     }
   });
-  watch('./app/index.html', ()=> {
+
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
 
-  watch('./app/assets/styles/**/*.css', ()=> {
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
 
 });
 
-gulp.task('cssInject',['styles'],()=>{
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
-      .pipe(browserSync.stream());
+    .pipe(browserSync.stream());
+
 });
